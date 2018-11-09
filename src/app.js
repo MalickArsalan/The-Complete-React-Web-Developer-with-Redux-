311acef269499d7409bb27ce027b10cb891cf332
Malick Arsalan Office <malickarsalan@hotmail.com>
2018-11-09 18:11:11 +0500

    Let and Const

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 console.log("App.js is running!");
 
-
 // JSX - Jaavscript XML
 /*
 var template = React.createElement(
@@ -10,17 +9,53 @@ var template = React.createElement(
 );
 */
 
- var template =( 
-<div>
-     <h1>Indecision App</h1>
-     <p>This is some info</p>
-     <ol>
-         <li>Item one</li>
-         <li>Item two</li>
-         <li>Item three</li>
-     </ol>
-</div>
+var app ={
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of a computer',
+    options: ['One','Two']
+
+};
+var template = (
+    <div>
+        <h1>{app.title}</h1>
+        {app.subtitle  && <p>{app.subtitle}</p>}
+        <p>{app.options.length > 0? 'Here are your options':'No options'}</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+    </div>
 );
+
+var user = { // We can use Objects in JSX expression
+    name: 'Andrew',
+    age: 26,
+    location: 'Philadelphia'
+};
+// var userName = 'Andrew';
+// var userAge = 26;
+// var userLoaction='New York';
+//  <h1>{userName}</h1> -> JSX Expression
+
+function getLocation(location) {
+    if (location){
+        // return location;
+        return  <p>Location: {location}</p>
+    } else {
+        return undefined;
+    }
+}
+
+var templateTwo = (
+    <div>
+        <h1>{user.name ? user.name.toUpperCase() + '!' : 'Anonymous'.toUpperCase()}</h1> 
+        {/* <p>Age: {user.age}</p> */}
+        {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+        {/* <p>Location: {getLocation(user.location)}</p> */}
+        {getLocation(user.location)}
+    </div>
+);
+
 
 var appRoot = document.getElementById('app');
 
--- src/playground/es6-let-const.js
+++ src/playground/es6-let-const.js
@@ -1,17 +1,16 @@
-'use strict';
-
 var nameVar = 'Malick Arsalan';
 var nameVar = 'Malick Qutub'; // Override Previous value
 console.log('nameVar = ', nameVar);
 
-var nameLet = 'Jan';
+let nameLet = 'Jan';
 // let nameLet = 'Julie'; // Cannot redefine
-nameLet = 'Julie'; // But can reassign
+nameLet = 'Julie';  // But can reassign
 console.log('nameLet = ', nameLet);
 
-var nameConst = 'Frank';
+const nameConst = 'Frank';
 // const nameConst = 'Gunther'; // Cannot redefine
 // nameConst = 'Gunther'; // Cannot reassign
 console.log('nameConst = ', nameConst);
+
 
 // var,let and const has functional scope which means we cannot acces it outside of function
